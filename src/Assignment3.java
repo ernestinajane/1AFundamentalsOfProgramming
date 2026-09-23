bf44b53f337a2ec1490e77441a0fa81141daa349
ernestinajane <delacruzernestinajane@gmail.com>
2026-09-24 01:11:24 +0800

    Add Assignment 3 and clean repository layout

---
+++ src/Assignment3.java
@@ -0,0 +1,19 @@
+public class Assignment3 {
+    public static void main (String[] args) {
+        //Provide Personal Information and assign them to variables with appropriate data
+        String firstName = "Ernestina Jane";
+        String lastName = "Dela Cruz";
+        String address = "Asingan Pangasinan";
+        String mobileNumber = "09812618242";
+        byte age = 18;
+        double gwa = 96.00;
+        //Display the output using java
+        System.out.println("First Name:" + firstName);
+        System.out.println("Last Name:" + lastName);
+        System.out.println("Address:" + address);
+        System.out.println("Mobile Number:"+ mobileNumber);
+        System.out.println("Age:"+ age);
+        System.out.println("GWA:"+ gwa);
+
+    }
+}
